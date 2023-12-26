ffab706d0a08bb3a3e8eb0bd0d9717fb883cd301
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2023-12-26 15:37:13 -0500

    Create extensions.py

---
+++ flaskblog/extensions.py
@@ -0,0 +1,7 @@
+"""Extensions"""
+from flask_sqlalchemy import SQLAlchemy
+#adds support for sqlalchemy to use with flask 
+from flask_login import LoginManager
+#user session mangement 
+db = SQLAlchemy()
+login_manager = LoginManager()
