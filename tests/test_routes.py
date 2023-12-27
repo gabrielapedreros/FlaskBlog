f08f147a1a45f6960d327e79035e3c54951e5125
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2023-12-27 14:40:01 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,25 +1,33 @@
-# test_routes.py
+""" Testing routes file """
 import pytest
+# pytest is a testing framework 
 from flaskblog import app, db
+# import Flask application instance 
+# import database instance from Flask app
 from flaskblog.models import User
+# import the users model
 
 @pytest.fixture(scope='module')
 def test_client():
+    """ This fixture function is ran before the test functions, where test_client is set up once 
+    before the first test and torn down after last test, as described by scope='module' """
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # In-memory database
-    app.config['SECRET_KEY'] = 'my_precious_secret_key'  # Set a secret key for testing
-    with app.app_context():
-        db.create_all()
-        yield app.test_client()
-        db.drop_all()
+    # set Flask app to testing mode 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # use in-memory SQLite database
+    app.config['SECRET_KEY'] = 'my_precious_secret_key'  # set a secret key for testing
+    with app.app_context():    # commands used to connect to database
+        db.create_all()        # create database tables defined in models
+        yield app.test_client() # this fixture ensures a clean state for each test function
+        db.drop_all()          # clean up database 
 
 def test_home_page(test_client):
-    response = test_client.get('/')
-    assert response.status_code == 200
-    assert b'Home' in response.data  
+    """ Test client request to app """
+    response = test_client.get('/') #send get request to home page of Flask app
+    assert response.status_code == 200    #loaded successfully
+    assert b'Home' in response.data  #ensure home page exists
 
 def test_about_page(test_client):
-    response = test_client.get('/about')
-    assert response.status_code == 200
-    assert b'About' in response.data  # Replace 'About' with actual text expected on your about page
-
+    """ Test client request to app """
+    response = test_client.get('/about') #send get request to home page of Flask app
+    assert response.status_code == 200    #loaded successfully
+    assert b'About' in response.data  #ensure about page exists
