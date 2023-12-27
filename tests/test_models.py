1383c6810f966350fe92ff8c00b0aa45119a22f7
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2023-12-27 14:32:09 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,16 @@
 """ Testing model file """
-import pytest # pytest is a testing framework 
-from flaskblog import app, db # import Flask application instance, import database instance from Flask app
-from flaskblog.models import NewsItem, User, NewsItemLike # import the users
+import pytest 
+# pytest is a testing framework 
+from flaskblog import app, db
+# import Flask application instance 
+# import database instance from Flask app
+from flaskblog.models import NewsItem, User, NewsItemLike
+# import the users
 
-""" This fixture function is ran before the test functions, where test_app is set up once before the first 
-test and torn down after last test, as described by scope='module' """
 @pytest.fixture(scope='module')
 def test_app():
+    """ This fixture function is ran before the test functions, where test_app is set up once 
+    before the first test and torn down after last test, as described by scope='module' """
     app.config['TESTING'] = True
     # set Flask app to testing mode 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # use in-memory SQLite database
@@ -15,31 +19,31 @@ def test_app():
         yield app              # this fixture ensures a clean state for each test function
         db.drop_all()          # clean up database 
         
-""" This fixture function is ran once before each test function, as described by scope='function'. test_db 
- depends on test_app """
 @pytest.fixture(scope='function')
 def test_db(test_app):
+    """ This fixture function is ran once before each test function, as described by 
+    scope='function'. test_db depends on test_app """
     # this fixture ensures a clean state for each test function
     yield db
 
-""" Test function for users in database """
 def test_user_creation(test_db):
+    """ Test function for users in database """
     user = User(username='testuser', email='test@example.com', auth0_user_id='auth0|testuser')
     test_db.session.add(user)
     test_db.session.commit()
     assert User.query.filter_by(username='testuser').first() is not None
     # ensures that the user exists
 
-""" Test function for news items in database """
 def test_news_item_creation(test_db):
+    """ Test function for news items in database """
     news_item = NewsItem(title='Test News', url='http://example.com', by='author')
     test_db.session.add(news_item)
     test_db.session.commit()
     assert NewsItem.query.filter_by(title='Test News').first() is not None
     # ensures that the news item exists
 
-""" Test function for which users like a news item in database """
 def test_news_item_like_creation(test_db):
+    """ Test function for which users like a news item in database """
     user = User(username='liker', email='liker@example.com', auth0_user_id='auth0|liker')
     news_item = NewsItem(title='Likeable News', url='http://likable.com', by='author')
     test_db.session.add(user)
