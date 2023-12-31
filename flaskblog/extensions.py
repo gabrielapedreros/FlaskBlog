1b499489991df7a21f704196f176602a0c94d8aa
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2023-12-31 16:33:40 -0500

    Update extensions.py

--- flaskblog/extensions.py
+++ flaskblog/extensions.py
@@ -1,8 +1,6 @@
 # Gabriela Pedreros - gp21j
-"""Extensions"""
-from flask_sqlalchemy import SQLAlchemy
-#adds support for sqlalchemy to use with flask 
-from flask_login import LoginManager
-#user session mangement 
-db = SQLAlchemy()
-login_manager = LoginManager()
+""" Extensions for database handling and user management """
+from flask_sqlalchemy import SQLAlchemy  #adds support for sqlalchemy to use with flask 
+from flask_login import LoginManager    #user session mangement 
+db = SQLAlchemy()                        #initialize database connection
+login_manager = LoginManager()          #initialize user management 
