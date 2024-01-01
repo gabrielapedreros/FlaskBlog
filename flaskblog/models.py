809856f872340fed08cf8b6f588624fe342cb7d2
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2024-01-01 14:00:49 -0500

    Update models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,17 +1,15 @@
 # Gabriela Pedreros - gp21j
-"""Models imports and libraries"""
-from flask_login import UserMixin
-#check if user is authenticated
-from flaskblog import db
-#import database to work on it 
-#create NewItem model, which will hold the hackernews post information
-class NewsItem(db.Model):
-    """NewsItems come from HackerNewsPortal"""
+""" This file defines database models using SQLAlchemy. Flask-Login is used for
+    user management """
+from flask_login import UserMixin        #used in user model to check if user is authenticated
+from flaskblog import db                #import database to work on it 
+
+class NewsItem(db.Model):        #create NewItem model, which will hold the hackernews post information
+    """ NewsItems class contains all News Item data """
     id = db.Column(db.Integer, primary_key=True)
     by = db.Column(db.String(100))
     descendants = db.Column(db.Integer)
-    # Assume 'kids' is a string representing a list
-    kids = db.Column(db.String(300))
+    kids = db.Column(db.String(300))    # Assume 'kids' is a string representing a list
     score = db.Column(db.Integer)
     text = db.Column(db.Text)
     time = db.Column(db.Integer)
@@ -20,21 +18,20 @@ class NewsItem(db.Model):
     url = db.Column(db.String(200))
     def __repr__(self):
         return f"NewsItem('{self.title}', '{self.url}')"
-#create User model which will hold all information about a user
-class User(db.Model, UserMixin):
-    """User class contains all user data"""
+
+class User(db.Model, UserMixin):    #create User model which will hold all information about a user
+    """ User class contains all user data """
     id = db.Column(db.Integer, primary_key=True)
     auth0_user_id = db.Column(db.String(120), unique=True, nullable=False)  # Auth0 User ID
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-#setting all users to be an admin
-    is_admin = db.Column(db.Boolean, default=True)
+    is_admin = db.Column(db.Boolean, default=True)        #setting all users to be an admin
     def __repr__(self):
         """Important user data returned"""
         return f"User('{self.username}', '{self.email}')"
-#NewItemLike model to see the relationship between a user and the post
-class NewsItemLike(db.Model):
-    """Who likes posts"""
+
+class NewsItemLike(db.Model):    #NewItemLike model to see the relationship between a user and the post
+    """ NewsItemLike class contains all user/post interaction data """
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     user = db.relationship('User', backref='likes')
     news_item_id = db.Column(db.Integer, db.ForeignKey('news_item.id'), primary_key=True)
